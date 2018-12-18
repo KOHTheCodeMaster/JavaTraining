2b1f76ad4396b1849e41663524dd5e57f18930c2
KOH <kohthecodemaster@gmail.com>
2018-12-18 18:00:18 +0530

    11th Commit. [HashSet]
    HashSet adds only unique elements i.e. it simply ignores any
    duplicate elements & doesn't adds them into the HashSet.
    
    Course: Advanced Java Programming - LinkedIn Learning..!!
    Mentor: David Gassner.
    
    Time Stamp: 18th December 2K18 05:59 PM..!!
    Code Developed By,
    ~K.O.H..!! ^__^
    
    Signed-off-by: KOH <kohthecodemaster@gmail.com>

--- src/dev/koh/AdvancedJavaTraining/Java7NewFeatures/Main.java
+++ src/dev/koh/AdvancedJavaTraining/Java7NewFeatures/Main.java
@@ -6,7 +6,27 @@ public class Main {
 
         System.out.println("Begin.\n");
 
-        ReflectionAPI obj = new ReflectionAPI();
+        UsingHashSet obj = new UsingHashSet();
+
+        Person person = new Person();
+        Student student = new Student();
+
+        //  HashSet adds only unique elements i.e. it simply ignores any
+        //  duplicate elements & doesn't adds them into the HashSet.
+        obj.hashSet.add(person);
+        obj.hashSet.add(student);
+        obj.hashSet.add(person);
+
+        System.out.println("Size: " + obj.hashSet.size());
+
+        obj.hashSet.add(null);
+        System.out.println("Size: " + obj.hashSet.size());
+
+        obj.hashSet.add(student);
+        System.out.println("Size: " + obj.hashSet.size());
+
+        obj.hashSet.remove(student);
+        System.out.println("Size: " + obj.hashSet.size());
 
         System.out.println("\nEnd.");
 
@@ -17,7 +37,7 @@ public class Main {
  *  Course: Advanced Java Programming - LinkedIn Learning..!!
  *  Mentor: David Gassner.
  *  Date Created: 15th December 2K18 04:58 PM..!!
- *  Last Modified: 18th December 2K18 05:33 PM..!!
+ *  Last Modified: 18th December 2K18 05:59 PM..!!
  *
  *  Code Developed By,
  *  ~K.O.H..!! ^__^
---
+++ src/dev/koh/AdvancedJavaTraining/Java7NewFeatures/UsingHashSet.java
@@ -0,0 +1,12 @@
+package dev.koh.AdvancedJavaTraining.Java7NewFeatures;
+
+import java.util.HashSet;
+
+public class UsingHashSet {
+
+    HashSet<Person> hashSet;
+
+    UsingHashSet() {
+        hashSet = new HashSet<>();
+    }
+}
