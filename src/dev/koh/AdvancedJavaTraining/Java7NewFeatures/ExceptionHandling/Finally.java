300fc78b920b0f087d74c83ee38163fde0cf2999
KOH <kohthecodemaster@gmail.com>
2018-12-29 13:31:33 +0530

    {AJT} 16th Commit. [Finally]
    Time Stamp: 29th December 2K18, 01:31 PM..!!
    
    Finally keyword is used to ensure the streams are closed safely.
    
    /*
     *  Time Stamp: 29th December 2K18, 12:58 PM..!!
     *
     *  Latest Update:  Finally keyword.
     *
     *  Course: Advanced Java Programming - LinkedIn Learning..!!
     *  Mentor: David Gassner.
     *
     *  Code Developed By,
     *  ~K.O.H..!! ^__^
     */
    
    Signed-off-by: KOH <kohthecodemaster@gmail.com>

---
+++ src/dev/koh/AdvancedJavaTraining/Java7NewFeatures/ExceptionHandling/Finally.java
@@ -0,0 +1,42 @@
+package dev.koh.AdvancedJavaTraining.Java7NewFeatures.ExceptionHandling;
+
+import java.io.*;
+
+public class Finally {
+
+    private FileReader fileReader = null;
+    private FileWriter fileWriter = null;
+    private BufferedReader bufferedReader = null;
+
+    void handleExceptionsUsingFinally() {
+
+        try {
+            fileReader = new FileReader("./res/txtfiles/testDoc.txt");
+            fileWriter = new FileWriter("./res/txtfiles/textDoc.txt");
+            bufferedReader = new BufferedReader(fileReader);
+            String temp;
+
+            while ((temp = bufferedReader.readLine()) != null) {
+                fileWriter.write(temp);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fileWriter != null)
+                    fileWriter.close();
+                if (fileReader != null)
+                    fileReader.close();
+                if (bufferedReader != null)
+                    bufferedReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
---
+++ src/dev/koh/AdvancedJavaTraining/Java7NewFeatures/ExceptionHandling/Main.java
@@ -0,0 +1,17 @@
+package dev.koh.AdvancedJavaTraining.Java7NewFeatures.ExceptionHandling;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Begin.");
+
+        Finally obj = new Finally();
+
+        obj.handleExceptionsUsingFinally();
+
+        System.out.println("\nEnd.");
+
+    }
+
+}
